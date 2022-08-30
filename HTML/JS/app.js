9b33ffcbe52cdb55b6bb1c2282e3600cabb97981
Arturo0214 <osvaldosuarezcruz@gmail.com>
2022-08-30 16:51:56 -0500

    first commit

---
+++ HTML/JS/app.js
@@ -0,0 +1,80 @@
+const modal = document.querySelector("#modal");
+const rName = document.querySelector("#restaurant-name");
+const rAddress = document.querySelector("#restaurant-address");
+const rPhone = document.querySelector("#restaurant-phone");
+const rImage = document.querySelector("#restaurant-image");
+const btnFavorites = document.querySelector("#restaurant-add-favorites");
+const favoriteStorage = localStorage.getItem("@favoritos");
+
+let restaurantes = [];
+
+
+let render = (restaurantsArr) => {
+    const results = document.querySelector("#results");
+    results.innerHTML = "";
+  
+    for (let i in restaurantsArr) {
+      let col = document.createElement("div");
+      col.classList.add("column", "is-4-desktop", "is-6-tablet");
+  
+      let card = document.createElement("div");
+      card.classList.add("card");
+      card.setAttribute("data-id", restaurantsArr[i].id);
+  
+      card.innerHTML = `<div class="card-image">
+                  <figure class="image is-16by9">
+                      <img src="${restaurantsArr[i].image}" alt="Placeholder image">
+                  </figure>
+              </div>
+              <div class="card-content">
+                  <span class="tag is-danger">${restaurantsArr[i].localtype} </span>
+                  <div class="is-flex is-justify-content-space-between is-align-items-center">
+                      <div class="content mb-0">
+                          <p class="has-text-weight-semibold mb-0 is-size-4 is-uppercase is-size-4 is-uppercase">${restaurantsArr[i].name}</p>
+                          <p class="mb-0 is-size-7">${restaurantsArr[i].ubicacion}</p>
+                      </div>
+                      <div class="ranking has-text-right">
+                          <span class="icon-text">
+                              <span  class="icon has-text-info">
+                              <i data-favorite="${restaurantsArr[i].id}" class="mdi mdi-star mdi-18px is-active"></i>
+                              </span> 
+                              <span> ${restaurantsArr[i].calificacion}</span>
+                          </span>
+                      </div>
+                  </div>
+              </div>
+              `;
+        card.addEventListener("click", () => mostrarModal(card.dataset.id));
+        col.append(card);
+        document.querySelector("#results").append(col);
+    }
+};
+
+const llenarModal = (restaurante) => {
+    const {name, ubicacion, image} = restaurante.data;
+    rName.innerText = name;
+    rAddress.innerText = ubicacion;
+    rImage.setAttribute("src", image);
+};
+  
+const mostrarModal = (id) => {
+    rName.innerText = "";
+    rAddress.innerText = "";
+    btnFavorites.setAttribute("id", id);
+    modal.classList.toggle("is-active");
+    if (id !== undefined) {
+      fetch("js/restaurantes.json${id}")
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data)
+        });
+    }
+  };
+
+
+fetch("js/restaurantes.json")
+.then(response => response.json())
+.then((data) => {
+  restaurantes = data;
+  render(data);
+}); 
---
+++ HTML/JS/filters.js
@@ -0,0 +1,69 @@
+let buscar = () => {
+    let nombres = document.querySelector("#inputBusqueda").value.toLowerCase();
+    let filtrados = restaurantes.filter((restaurante) => {
+      return restaurante.name.toLowerCase().includes(nombres);
+    });
+    render(filtrados);
+};
+
+let reviews = () =>{
+    let filtro = restaurantes.filter((restaurante) => {
+      return restaurante.calificacion >= 2;
+    });
+    render(filtro);
+};
+
+let buscarCategoria = (category) => {
+    let filtro = restaurantes.filter((restaurante) => {
+        return restaurante.localtype == category;
+    });
+    render(filtro);
+};
+
+
+document
+.querySelector("#busqueda button")
+.addEventListener("click", buscar);
+
+document
+.querySelector("#inputBusqueda")
+.addEventListener("keyup", buscar);
+
+document
+.querySelector("#todos")
+.addEventListener("click", () => render(restaurantes));
+
+document
+  .querySelector("#restaurantes")
+  .addEventListener("click", () => buscarCategoria("restaurante"));
+
+document
+  .querySelector("#fast-food")
+  .addEventListener("click", () => buscarCategoria("fast-food"));
+
+document
+  .querySelector("#abarrotes")
+  .addEventListener("click", () => buscarCategoria("abarrotes"));
+
+document
+  .querySelector("#mandaditos")
+  .addEventListener("click", () => buscarCategoria("local"));
+
+document
+  .querySelector("#farmacia")
+  .addEventListener("click", () => buscarCategoria("farmacia"));
+
+document
+.querySelector("#alfabeto")
+.addEventListener("click",() => {
+  restaurantes.sort(function(a,b){
+      return a.name > b.name ? 1 : -1;
+  });
+ render(restaurantes);
+})
+
+document.querySelector("#calificacion").addEventListener("click",()=>{
+  restaurantes.sort((a,b) => a.reviews < b.reviews ? 1 : -1);
+  render(restaurantes);
+});
+
